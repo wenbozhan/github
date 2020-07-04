0329a3928637af94041fa44700d6d56663ba67ce
wenbozhan <w2679453225@foxmail.com>
2020-07-04 10:53:23 +0800

    test

--- s2.cpp
+++ s2.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <string>
+
 using namespace std;
+
 int main()
 {
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
